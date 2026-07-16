3034a2c2f01d01442300ceaf838ecd06fe01c602
mohhitbishnoi <mohitbishnoi1762@gmail.com>
2026-07-16 11:44:30 +0530

    commit changes

---
+++ Domain/Entitis/Enums/TicketPriority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entitis.Enums;
+
+public enum TicketPriority
+{
+    Critical = 1,
+    High = 2,
+    Medium = 3,
+    Low = 4
+}

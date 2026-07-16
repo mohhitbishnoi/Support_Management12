090ce0f476d847550aa860126b56862cf49ceb69
mohhitbishnoi <mohitbishnoi1762@gmail.com>
2026-07-16 10:21:42 +0530

    SLA Settings

---
+++ Application/Features/SLASettingsFeature/Commands/Queries/GetAllSLASettingsQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces.Repository;
+using Domain.Entitis;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+
+namespace Application.Features.SLASettings.Queries;
+
+public class GetAllSLASettingsQuery : IRequest<Result<List<SlASetting>>>
+{
+    internal class GetAllSLASettingsQueryHandler
+        : IRequestHandler<GetAllSLASettingsQuery, Result<List<SlASetting>>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllSLASettingsQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<List<SlASetting>>> Handle(
+            GetAllSLASettingsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var slaSettings = await _unitOfWork.Repository<SlASetting>()
+                .Entities
+                .AsNoTracking()
+                .OrderBy(x => x.Id)
+                .ToListAsync(cancellationToken);
+
+            if (slaSettings == null || !slaSettings.Any())
+            {
+                return Result<List<SlASetting>>.BadRequest("No SLA Settings found.");
+            }
+
+            return Result<List<SlASetting>>.Success(slaSettings,"");
+        }
+    }
+}
---
+++ Application/Features/SLASettingsFeature/Commands/Queries/GetSLASettingByIdQuery.cs
@@ -0,0 +1,45 @@
+﻿using Application.Interfaces.Repository;
+using Domain.Entitis;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+
+namespace Application.Features.SLASettings.Queries;
+
+public class GetSLASettingByIdQuery : IRequest<Result<SlASetting>>
+{
+    public int Id { get; set; }
+
+    public GetSLASettingByIdQuery(int id)
+    {
+        Id = id;
+    }
+
+    internal class GetSLASettingByIdQueryHandler
+        : IRequestHandler<GetSLASettingByIdQuery, Result<SlASetting>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetSLASettingByIdQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<SlASetting>> Handle(
+            GetSLASettingByIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var slaSetting = await _unitOfWork.Repository<SlASetting>()
+                .Entities
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (slaSetting == null)
+            {
+                return Result<SlASetting>.BadRequest("SLA Setting not found.");
+            }
+
+            return Result<SlASetting>.Success(slaSetting,"");
+        }
+    }
+}
